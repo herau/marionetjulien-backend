ece0883971e53642c68dad6b9eb04d63b48e463e
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2017-02-27 11:14:15 +0100

    setup mail service

--- src/main/java/org/marionetjulien/controllers/FormController.java
+++ src/main/java/org/marionetjulien/controllers/FormController.java
@@ -2,7 +2,10 @@ package org.marionetjulien.controllers;
 
 import org.marionetjulien.domains.Form;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -13,9 +16,22 @@ import javax.validation.Valid;
 @RestController
 public class FormController {
 
+    private final MailSender mailSender;
+
+    public FormController(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @PostMapping("/api/form")
-    public ResponseEntity postForm(@Valid Form form) {
+    public ResponseEntity postForm(@Valid @RequestBody Form form) {
         System.out.println(form);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("leboulanger.aurelien@gmail.com");
+        mailMessage.setSubject("Confirmation présence mariage");
+
+        mailSender.send(mailMessage);
+
         return ResponseEntity.ok().build();
     }
 
