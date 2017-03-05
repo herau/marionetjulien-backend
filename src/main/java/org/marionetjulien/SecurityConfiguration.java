f9da5a7898d5b350fa41a98b9e94a0eeff644a04
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2017-03-05 10:32:15 +0100

    enable cors for spring security

---
+++ src/main/java/org/marionetjulien/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package org.marionetjulien;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by n27 on 3/5/17.
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+    }
+}
